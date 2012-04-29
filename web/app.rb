4ca75f8dd9c5f786bcf24ec4dfc311d48e442227
Kenichi Nakamura <kenichi.nakamura@gmail.com>
2012-04-29 14:21:38 -0700

    add env.rb

--- web/app.rb
+++ web/app.rb
@@ -4,30 +4,6 @@ Bundler.require :development if development?
 
 configure do
 
-  CONFIG = YAML.load_file(File.join(File.dirname(__FILE__), 'config.yml'))
-
-  Sqoot.configure do |config|
-    config.affiliate_token = CONFIG['sqoot']['affiliate_token']
-  end
-
-  CATEGORIES = {
-    "Auto" =>             'automotive-services',
-    "Bars + Clubs" =>     'bars-clubs',
-    "Bowling" =>          'bowling',
-    "Cleaning" =>         'home-services',
-    "Concerts" =>         'concerts',
-    "Dental" =>           'dental',
-    "Massage" =>          'massage',
-    "Men's Clothing" =>   'mens-clothing',
-    "Museums" =>          'museums',
-    "Outdoors" =>         'outdoor-adventures',
-    "Photography" =>      'photography-services',
-    "Restaurants" =>      'restaurants',
-    "Sporting Events" =>  'sporting-events',
-    "Theatre" =>          'theater',
-    "Women's Clothing" => 'womens-clothing'
-  }
-
   set :geoloqi_client_id,     CONFIG['geoloqi']['client_id']
   set :geoloqi_client_secret, CONFIG['geoloqi']['client_secret']
 
