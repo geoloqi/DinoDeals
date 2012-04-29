d5b7c6493f6a57610c9f236d787584b1ec6250d4
Kenichi Nakamura <kenichi.nakamura@gmail.com>
2012-04-29 14:13:09 -0700

    add category map

--- web/app.rb
+++ web/app.rb
@@ -10,6 +10,24 @@ configure do
     config.affiliate_token = CONFIG['sqoot']['affiliate_token']
   end
 
+  CATEGORIES = {
+    "Auto" =>             'automotive-services',
+    "Bars + Clubs" =>     'bars-clubs',
+    "Bowling" =>          'bowling',
+    "Cleaning" =>         'home-services',
+    "Concerts" =>         'concerts',
+    "Dental" =>           'dental',
+    "Massage" =>          'massage',
+    "Men's Clothing" =>   'mens-clothing',
+    "Museums" =>          'museums',
+    "Outdoors" =>         'outdoor-adventures',
+    "Photography" =>      'photography-services',
+    "Restaurants" =>      'restaurants',
+    "Sporting Events" =>  'sporting-events',
+    "Theatre" =>          'theater',
+    "Women's Clothing" => 'womens-clothing'
+  }
+
 end
 
 get '/' do
