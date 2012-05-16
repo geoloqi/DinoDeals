b4a734f8bbd9111f41410cbc2b129d5006a454b2
Patrick Arlt <patrick.arlt@gmail.com>
2012-05-16 14:24:57 -0700

    add sample config file

---
+++ Titanium/Resources/config-template.js
@@ -0,0 +1,6 @@
+exports = {
+  clientId: "get your clientId at http://developers.geoloqi.com",
+  clientSecret: "get your clientSecret at http://developers.geoloqi.com",
+  baseURL: "where the dinodeals server is hosted"
+}
+
