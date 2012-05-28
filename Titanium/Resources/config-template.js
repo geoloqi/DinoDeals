d1c85f9443d4e3b673d9693da0fe399e7d4d06a6
Patrick Arlt <patrick.arlt@gmail.com>
2012-05-28 15:50:28 -0700

    add pushIcon to config template

--- Titanium/Resources/config-template.js
+++ Titanium/Resources/config-template.js
@@ -2,6 +2,7 @@ Config = {
   clientId: "get your clientId at http://developers.geoloqi.com/account/applications",
   clientSecret: "get your clientSecret at http://developers.geoloqi.com/account/applications",
   pushAccount: "example@gmail.com",
+  pushIcon: "path_to_icon_for_android_push",
   baseURL: "where the dinodeals server is hosted"
 };
 
