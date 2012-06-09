8bfa5872d6e18685c0977b6b04f0369ef0a88059
Aaron Parecki <aaron@parecki.com>
2012-06-09 00:30:48 -0700

    Patch Geoloqi module until pull request #3 is merged into gem

--- web/env.rb
+++ web/env.rb
@@ -43,3 +43,51 @@ Geoloqi.config :client_id => CONFIG['geoloqi']['client_id'], :client_secret => C
 def get_layers
   Geoloqi::Session.new(:access_token => CONFIG['geoloqi']['app_access_token']).get('layer/list')[:layers]
 end
+
+
+
+
+
+# Remove this when the response attr is added back to the gem
+# See https://github.com/geoloqi/geoloqi-ruby/pull/3
+
+module Geoloqi
+  class Session
+    attr_reader :response
+
+    def run(meth, path, query=nil, headers={})
+      renew_access_token! if auth[:expires_at] && Time.rfc2822(auth[:expires_at]) <= Time.now && !(path =~ /^\/?oauth\/token$/)
+      retry_attempt = 0
+
+      begin
+        response = execute meth, path, query, headers
+        @response = response # Make the response object available to the caller
+        hash = JSON.parse response.body, :symbolize_names => @config.symbolize_names
+
+        if hash.is_a?(Hash) && hash[:error] && @config.throw_exceptions
+          if @config.use_dynamic_exceptions && !hash[:error].nil? && !hash[:error].empty?
+            exception_class_name = hash[:error].gsub(/\W+/, '_').split('_').collect {|w| w.capitalize}.join+'Error'
+            Geoloqi.const_set exception_class_name, Class.new(Geoloqi::ApiError) unless Geoloqi.const_defined? exception_class_name
+            raise_class = Geoloqi.const_get exception_class_name
+          else
+            raise_class = ApiError
+          end
+          raise raise_class.new(response.status, hash[:error], hash[:error_description])
+        end
+      rescue Geoloqi::ApiError
+        raise Error.new('Unable to procure fresh access token from API on second attempt') if retry_attempt > 0
+        if hash[:error] == 'expired_token' && !(hash[:error_description] =~ /The auth code expired/)
+          renew_access_token!
+          retry_attempt += 1
+          retry
+        else
+          fail
+        end
+      rescue JSON::ParserError
+        raise Geoloqi::Error, "API returned invalid JSON. Status: #{response.status} Body: #{response.body}"
+      end
+      @config.use_hashie_mash ? Hashie::Mash.new(hash) : hash
+    end
+
+  end
+end
